2ee4c9987789cd77c0059bff1738b1c2222a8e32
shuyossy <shuyossy220@gmail.com>
2024-12-20 10:46:29 +0900

    chore: ドキュメント更新

--- src/main/java/sak/metricstool/repository/SettingsRepository.java
+++
@@ -1,7 +0,0 @@
-package sak.metricstool.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sak.metricstool.entity.Settings;
-
-public interface SettingsRepository extends JpaRepository<Settings, String> {
-}
